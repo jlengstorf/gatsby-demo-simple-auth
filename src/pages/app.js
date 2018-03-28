49cc8c2739cb34f2e8afcd336d82c0fe84b07e65
Jason Lengstorf <jason@lengstorf.com>
2018-03-27 23:10:03 -0700

    fix: remove the router component

--- src/pages/app.js
+++ src/pages/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'gatsby-link';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import Details from '../components/Details';
 import Home from '../components/Home';
 import Login from '../components/Login';
@@ -8,12 +8,10 @@ import PrivateRoute from '../components/PrivateRoute';
 import Status from '../components/Status';
 
 export default () => (
-  <Router>
-    <div>
-      <Status />
-      <PrivateRoute path="/app/profile" component={Home} />
-      <PrivateRoute path="/app/details" component={Details} />
-      <Route path="/app/login" component={Login} />
-    </div>
-  </Router>
+  <div>
+    <Status />
+    <PrivateRoute path="/app/profile" component={Home} />
+    <PrivateRoute path="/app/details" component={Details} />
+    <Route path="/app/login" component={Login} />
+  </div>
 );
